ee5fe7bdd4a31183113f17c4b49683438b7ac4f7
lkamil <lkamil@realists.de>
2026-07-06 10:18:11 +0200

    Initial Commit

---
+++ SupabaseAuth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SupabaseAuth
+//
+//  Created by Laila Kamil on 06.07.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SupabaseAuth/SupabaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseAuthApp.swift
+//  SupabaseAuth
+//
+//  Created by Laila Kamil on 06.07.26.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
